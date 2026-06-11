1e94b62ea222619a0a266ea6b321389416e62f10
Luigi Pistone <luigipistone@iMac-di-Luigi.station>
2026-06-11 14:37:37 +0200

    Use Inter for liquid glass typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'SF Pro Text', ...defaultTheme.fontFamily.sans],
             },
         },
     },
