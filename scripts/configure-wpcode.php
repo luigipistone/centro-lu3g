08615de49a039537c7a7cb2af0c0c0cf0b68d2e8
Luigi Pistone <luigi.pistone@lu3g.it>
2026-07-23 16:43:51 +0200

    Provision WPCode snippets through WordPress data APIs

--- scripts/configure-wpcode.php
+++ scripts/configure-wpcode.php
@@ -1,6 +1,6 @@
 <?php
 
-if (! class_exists('WPCode_Snippet')) {
+if (! post_type_exists('wpcode')) {
     throw new RuntimeException('WPCode non è attivo.');
 }
 
@@ -33,17 +33,31 @@ foreach ($snippets as $definition) {
         'posts_per_page' => 1,
     ]);
 
-    $snippet = new WPCode_Snippet($existing ? $existing[0]->ID : []);
-    $snippet->title = $definition['title'];
-    $snippet->code = $definition['code'];
-    $snippet->code_type = $definition['type'];
-    $snippet->location = $definition['location'];
-    $snippet->auto_insert = 1;
-    $snippet->active = true;
+    $post = [
+        'post_type' => 'wpcode',
+        'post_status' => 'publish',
+        'post_title' => $definition['title'],
+        'post_content' => $definition['code'],
+    ];
 
-    if (! $snippet->save()) {
-        throw new RuntimeException('Impossibile salvare lo snippet '.$definition['title'].'.');
+    if ($existing) {
+        $post['ID'] = $existing[0]->ID;
+        $snippetId = wp_update_post($post, true);
+    } else {
+        $snippetId = wp_insert_post($post, true);
     }
+
+    if (is_wp_error($snippetId)) {
+        throw new RuntimeException('Impossibile salvare lo snippet '.$definition['title'].': '.$snippetId->get_error_message());
+    }
+
+    wp_set_object_terms($snippetId, $definition['type'], 'wpcode_type', false);
+    wp_set_object_terms($snippetId, $definition['location'], 'wpcode_location', false);
+    update_post_meta($snippetId, '_wpcode_auto_insert', 1);
+}
+
+if (function_exists('wpcode')) {
+    wpcode()->cache->cache_all_loaded_snippets();
 }
 
 WP_CLI::success('Snippet WPCode configurati.');
